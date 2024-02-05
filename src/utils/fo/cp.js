06fc84d2895172ffe28708450654f3a53ec2a02a
Artem Skorbezh <artyom.ireday@gmail.com>
2024-02-05 20:49:46 +0300

    fix: check files

--- src/utils/fo/cp.js
+++ src/utils/fo/cp.js
@@ -1,29 +1,44 @@
-import { createReadStream, createWriteStream } from "fs";
-import path from "path";
-import { __dirname } from "../../index.js";
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { __dirname } from '../../index.js';
+import fsPromises from 'fs/promises';
 
-const cp = ([pathToFile, pathToNewDir]) => {
-    return new Promise((resolve) => {
-        pathToFile = path.isAbsolute(pathToFile) 
-        ? pathToFile 
-        : path.join(__dirname, pathToFile);
+const cp = async ([pathToFile, pathToNewDir]) => {
+  try {
+    pathToFile = path.isAbsolute(pathToFile)
+      ? pathToFile
+      : path.join(__dirname, pathToFile);
 
-        pathToNewDir = path.isAbsolute(pathToNewDir) 
-        ? pathToNewDir 
-        : path.join(__dirname, pathToNewDir);
+    pathToNewDir = path.isAbsolute(pathToNewDir)
+      ? pathToNewDir
+      : path.join(__dirname, pathToNewDir);
 
+    await fsPromises.access(pathToFile);
 
-        const rs = createReadStream(pathToFile);
-        rs.on("error", () => resolve('\x1b[41m❌ Cp: Operation failed\x1b[0m'));
-        const fileName = pathToFile.slice(pathToFile.lastIndexOf(path.sep) + 1);
-        const ws = createWriteStream(path.join(pathToNewDir, fileName));
-        ws.on("error", () => resolve('\x1b[41m❌ Cp: Operation failed\x1b[0m'));
-        rs.pipe(ws);
-        rs.on('end', () => {
-            ws.close();
-            resolve()
-        });
-    })
-}
+    const rs = createReadStream(pathToFile);
+    rs.on('error', () => {
+      console.error('\x1b[41m❌ Cp: Read operation failed\x1b[0m');
+      throw new Error('Read operation failed');
+    });
+
+    const fileName = pathToFile.slice(pathToFile.lastIndexOf(path.sep) + 1);
+    const ws = createWriteStream(path.join(pathToNewDir, fileName));
+    ws.on('error', () => {
+      console.error('\x1b[41m❌ Cp: Write operation failed\x1b[0m');
+      throw new Error('Write operation failed');
+    });
+
+    rs.pipe(ws);
+
+    await new Promise((resolve) => {
+      rs.on('end', () => {
+        ws.close();
+        resolve();
+      });
+    });
+  } catch (error) {
+    console.error('\x1b[41m❌ Cp: Write operation failed\x1b[0m');
+  }
+};
 
 export default cp;
--- src/utils/fo/mv.js
+++ src/utils/fo/mv.js
@@ -1,14 +1,13 @@
-import { __dirname } from "../../index.js";
-import cp from "./cp.js";
-import remove from "./remove.js";
-
+import { __dirname } from '../../index.js';
+import cp from './cp.js';
+import remove from './remove.js';
 
 const mv = ([pathToFile, pathToNewDir]) => {
-    return new Promise((resolve) => {
-        cp([pathToFile, pathToNewDir]).then((result) => {
-            !!result ? resolve(result) : resolve(remove([pathToFile]))
-        })
-    })
-}
+  return new Promise((resolve) => {
+    cp([pathToFile, pathToNewDir]).then((result) => {
+      !!result ? resolve(result) : resolve(remove([pathToFile]));
+    });
+  });
+};
 
 export default mv;
